a4034345bd4a5426a2e72ffef05c52bebf9566a6
Adjiri <racim-mohamed.adjiri@epitech.eu>
2026-03-23 17:54:22 +0100

    add : create roomImage repository

---
+++ src/core/domain/cinema/room-image/port/room-image-repository.port.ts
@@ -0,0 +1,12 @@
+import { getAllResponse } from "src/core/domain/global/types/global.type";
+import { RoomImage } from "src/infrastructure/adapters/persistence/sql/entities/room-image.entity";
+
+
+export interface IRoomImageRepository {
+    create(roomImage : Partial<RoomImage>) : RoomImage;
+    findAll({page, size} : {page : number, size : number}) : Promise<getAllResponse<RoomImage>>;
+    findById(id : number) : Promise<RoomImage | null>;
+    update(id : number, roomImage : Partial<RoomImage>) : Promise<RoomImage | null>;
+    delete(id : number) : Promise<RoomImage | null>;
+    save(roomImage : RoomImage) : Promise<RoomImage>;
+}

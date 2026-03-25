e8655f486197d9bc24c6cac80212e83c0556195f
Adjiri <racim-mohamed.adjiri@epitech.eu>
2026-03-25 15:38:09 +0100

    fix : rename IRoomRepository by IRoomRepositoryPort

--- src/core/domain/cinema/room-image/port/room-image-repository.port.ts
+++ src/core/domain/cinema/room-image/port/room-image-repository.port.ts
@@ -2,7 +2,7 @@ import { getAllResponse } from "src/core/domain/global/types/global.type";
 import { RoomImage } from "src/infrastructure/adapters/persistence/sql/entities/room-image.entity";
 
 
-export interface IRoomImageRepository {
+export interface IRoomImageRepositoryPort {
     create(roomImage : Partial<RoomImage>) : RoomImage;
     findAll({page, size} : {page : number, size : number}) : Promise<getAllResponse<RoomImage>>;
     findById(id : number) : Promise<RoomImage | null>;

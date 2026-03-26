707b2cb94f04d5ba7f78d300aac2ca52095454ba
Adjiri <racim-mohamed.adjiri@epitech.eu>
2026-03-26 01:00:21 +0100

    fix : update room image module

--- src/modules/room-image/room-image.module.ts
+++ src/modules/room-image/room-image.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RoomImage } from "src/infrastructure/adapters/persistence/sql/entities/room-image.entity";
 import { RoomImageController } from "./room-image.controller";
-import { ROOM_IMAGE_REPOSITORY, ROOM_IMAGE_SERVICE } from "src/core/domain/global/token";
+import { ROOM_IMAGE_REPOSITORY, ROOM_IMAGE_SERVICE, STORAGE_PORT } from "src/core/domain/global/token";
 import { RoomImageService } from "src/core/application/cinema/room-image/room-image.service";
 import { SqlRoomImageRepository } from "src/infrastructure/adapters/persistence/sql/repositories/sql-room-image.repository";
+import { MinioService } from "src/infrastructure/adapters/external/minio.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([RoomImage])],
@@ -12,6 +13,7 @@ import { SqlRoomImageRepository } from "src/infrastructure/adapters/persistence/
     providers : [
         {provide : ROOM_IMAGE_SERVICE, useClass: RoomImageService},
         { provide: ROOM_IMAGE_REPOSITORY, useClass: SqlRoomImageRepository },
+        { provide: STORAGE_PORT, useClass: MinioService },
     ],
     exports : [ROOM_IMAGE_SERVICE]
 })

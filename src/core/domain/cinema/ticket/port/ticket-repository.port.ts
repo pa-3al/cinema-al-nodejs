1ad6a16be81dc3d3cc6c79d74891688bec1610a2
Adjiri <racim-mohamed.adjiri@epitech.eu>
2026-04-15 14:11:22 +0200

    add : ticket repository port

---
+++ src/core/domain/cinema/ticket/port/ticket-repository.port.ts
@@ -0,0 +1,10 @@
+import { Ticket } from "../../../../../infrastructure/adapters/persistence/sql/entities/ticket.entity";
+import { TicketUsage } from "../../../../../infrastructure/adapters/persistence/sql/entities/ticket-usage.entity";
+import { getAllResponse } from "../../../global/types/global.type";
+
+export interface ITicketRepositoryPort {
+    save(ticket: Partial<Ticket>): Promise<Ticket>;
+    findOneWithUsages(id: number): Promise<Ticket | null>;
+    findUserTickets(userId: string, { page, size }: { page: number; size: number }): Promise<getAllResponse<Ticket>>;
+    saveUsage(usage: Partial<TicketUsage>): Promise<TicketUsage>;
+}

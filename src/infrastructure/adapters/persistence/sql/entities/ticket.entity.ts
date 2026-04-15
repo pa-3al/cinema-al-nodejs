24ccb14a439dd58d1817803c5b42fba2bbffcbcf
Adjiri <racim-mohamed.adjiri@epitech.eu>
2026-04-16 01:53:00 +0200

    add : ticket entity

---
+++ src/infrastructure/adapters/persistence/sql/entities/ticket.entity.ts
@@ -0,0 +1,49 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { User } from "./user.entity";
+import { TicketUsage } from "./ticket-usage.entity";
+
+export enum TicketType {
+    SOLO = "solo",
+    TEN = "ten",
+}
+
+@Entity("ticket")
+export class Ticket {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        name: "ticket_type",
+        type: "varchar",
+        length: 10,
+        nullable: false,
+    })
+    ticketType: TicketType;
+
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: "updated_at" })
+    updatedAt: Date;
+
+    @DeleteDateColumn({ name: "deleted_at" })
+    deletedAt: Date;
+
+    @ManyToOne(() => User, (user) => user.tickets, { nullable: false })
+    @JoinColumn({ name: "user_id" })
+    user: User;
+
+    @OneToMany(() => TicketUsage, (usage) => usage.ticket)
+    usages: TicketUsage[];
+}

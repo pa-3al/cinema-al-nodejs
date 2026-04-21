57555b90fa05d02b4fffab643f66c515cda480e5
Rémy <remy.thibaut2005@gmail.com>
2026-04-21 21:43:20 +0200

    Add Transaction Dto

---
+++ src/core/domain/user/dto/transaction.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, Min } from "class-validator";
+
+export class MoneyOperationDto {
+    @ApiProperty({ description: "Montant de l'opération", example: 50 })
+    @IsNumber()
+    @Min(0.01)
+    amount: number;
+}
+
+export class TransactionDetailDto {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty({ description: "Type d'opération", example: "deposit" })
+    type: string;
+
+    @ApiProperty({ description: "Montant", example: 50 })
+    amount: number;
+
+    @ApiProperty()
+    createdAt: Date;
+}
